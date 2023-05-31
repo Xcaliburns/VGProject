dc86a24b69b80b089353addbfbfc6a83d5aa25a6
david abruzzo <david.abruzzo@gmail.com>
2023-05-31 12:32:12 +0200

    create and read ok

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-const db =require('./models'); // cet import est ici pour sequelize
+const db = require('./models'); // cet import est ici pour sequelize
 
+//Routers
+
+const postRouter= require ("./routes/Posts");
+app.use ("/posts",postRouter);
 
 db.sequelize.sync().then(()=>{
 app.listen(5000,()=>{
